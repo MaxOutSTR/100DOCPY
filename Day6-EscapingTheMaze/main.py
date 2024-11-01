a12234ce93a518fe76951296e0db495a03cd08c7
Guillermo Zumaran <gzumaranbazan@gmail.com>
2024-10-31 21:39:03 -0500

    add progress on main projects structure

---
+++ Day6-EscapingTheMaze/main.py
@@ -0,0 +1,10 @@
+# def my_function(): #Function declaration syntax
+#   print("Hi")
+
+# my_function() #Function invokation syntax
+
+# while something_true: #While loop structure
+#   print("Do something")
+
+#Today's project was a maze escaping challenge at another webpage, no code here :c
+
