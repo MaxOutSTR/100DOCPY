00a57758d7465197a964bffd25bde86f166b5aa4
Guillermo Zumaran <gzumaranbazan@gmail.com>
2024-11-12 19:29:11 -0500

    add boilerplate code from course

---
+++ Day16-CoffeeMachineOOP/main.py
@@ -0,0 +1,25 @@
+# from turtle import Turtle, Screen
+# from prettytable import PrettyTable
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("coral")
+
+# timmy.forward(100)
+
+# my_screen = Screen()
+
+# my_screen.exitonclick()
+
+#Creating new venv: py -m venv venv
+#Installing package from PyPI: pip install "packagename"
+
+# table = PrettyTable()
+
+# table.add_column("Pokemon name", ["Pikachu", "Charmander"])
+# table.add_column("Type", ["Electric", "Fire", "Plant"])
+
+# print(table)
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
