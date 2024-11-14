1f650f7002c2d1f2ab6aab2469a71bd17812a307
Guillermo Zumaran <gzumaranbazan@gmail.com>
2024-11-13 23:09:05 -0500

    add structure for project 17

---
+++ Day17-Quiz/main.py
@@ -0,0 +1,14 @@
+# class User:
+#   def __init__(self, email):
+#     self.email = email
+#     self.active = True
+#     print("New user being created...")
+  
+#   def report_email(self):
+#     print(self.email)
+
+# adam = User("sample@sampleinc.com")
+
+# adam.report_email()
+
+
